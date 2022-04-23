e7d06d2e860c89bb10d970926566827ed230efe7
Dalmo Veras <verasnt@protonmail.com>
2022-04-22 21:16:23 -0300

    add pixel_to_point fn

--- src/main.rs
+++ src/main.rs
@@ -23,13 +23,38 @@ fn parse_pair<T: FromStr>(s: &str, separator: char)-> Option<(T,T)>{
 	match s.find(separator){
 		None => None,
 		Some(index)=>{
-			match(T::fromstr(&s[..index]), T::from_str(&s[index +1..])){
+			match(T::from_str(&s[..index]), T::from_str(&s[index +1..])){
 				(Ok(l), Ok(r)) => Some((l,r)),
 				_=> None
 			}
 		}
 	}
 }
+
+fn parse_complex(s: &str) -> Option<Complex<f64>>{
+	match parse_pair(s,','){
+		Some((re, im))=> Some(Complex {re, im}),
+		None => None
+	}
+}
+
+fn pixel_to_point(bounds:(usize,usize),
+				  pixel:(usize,usize),
+				  upper_left: Complex<f64>,
+				  lower_right: Complex<f64>) -> Complex<f64>{
+
+	let (width, height) = (lower_right.re - upper_left.re, upper_left.im - lower_right.im);
+	Complex {
+		re: upper_left.re+pixel.0 as f64 * width /bounds.0 as f64,
+		im: upper_left.im - pixel.1 as f64 * height /bounds.1 as f64
+	}
+}
+
+fn main(){
+	
+}
+
+//Tests
 
 #[test]
 fn test_parse_pair(){
@@ -42,15 +67,15 @@ fn test_parse_pair(){
 	assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5,1.5)));
 }
 
-fn parse_complex(s: &str) -> Option<Complex<f64>>{
-	match parse_pair(s,','){
-		Some((re, im))=> Some(Complex {re, im}),
-		None => None
-	}
-}
-
 #[test]
 fn test_pair_complex(){
 	assert_eq!(parse_complex("1.25,-0.0625"),Some(Complex {re: 1.25, im:-0.0625}));
 	assert_eq!(parse_complex(",-0.0625"), None);
+}
+
+#[test]
+fn test_pixel_to_point(){
+	assert_eq!(pixel_to_point((100,200),(25,175), Complex {re: -1.0, im: 1.0},
+												Complex {re: 1.0, im: -1.0}),
+												Complex {re: -0.5, im: -0.75});
 }
