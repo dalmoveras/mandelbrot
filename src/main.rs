8391538d6b81d0db2c460aa7c67989a427c1f6ea
Dalmo Veras <verasnt@protonmail.com>
2022-04-28 19:33:53 -0300

    remove square_loop fn

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,6 @@ use std::str::FromStr;
 
 use std::env;
 
-fn complex_square_add_loop(c: Complex<f64>) {
-	let mut z = Complex { re: 0.0, im: 0.0 };
-	loop {
-		z = z * z + c;
-	}
-}
-
 fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
 	let mut z = Complex { re: 0.0, im: 0.0 };
 	for i in 0..limit {
